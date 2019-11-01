b5c237f828d2408fcf6123f56083422ae0ae1d61
Nijar <nijarr2020@gmail.com>
2019-11-01 23:01:21 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Navbar from '../../../../Desktop/App1/mario/src/components/layout/Navbar'
-import Dashboard from '../../../../Desktop/App1/mario/src/components/dashboard/Dashboard'
-import ProjectDetails from '../../../../Desktop/App1/mario/src/components/projects/ProjectDetails'
-import SignIn from '../../../../Desktop/App1/mario/src/components/auth/SignIn'
-import SignUp from '../../../../Desktop/App1/mario/src/components/auth/SignUp'
-import CreateProject from '../../../../Desktop/App1/mario/src/components/projects/CreateProject'
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
+import CreateProject from './components/projects/CreateProject'
 
 class App extends Component {
   render() {
